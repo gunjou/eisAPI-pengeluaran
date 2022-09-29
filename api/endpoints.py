8d2e51234b00ed447a09fb2f13777aec909b58ad
gunjou <gjoefandi@gmail.com>
2022-09-29 09:44:57 +0700

    add pengeluaran rekanan

--- api/endpoints.py
+++ api/endpoints.py
@@ -72,7 +72,6 @@ def tren_pengeluaran():
         else:
             tren[month_id[i]]['persentase_predict'] = round(((tren[month_id[i]]['tahun_ini'] - tren[month_id[i]]['tahun_selanjutnya'])
                                                              / tren[month_id[i]]['tahun_selanjutnya']) * 100, 2)
-
     data = {
         "judul": "Tren Pengeluaran",
         "label": 'Pengeluaran',
@@ -84,17 +83,82 @@ def tren_pengeluaran():
 
 @pengeluaran_bp.route('/pengeluaran_instalasi')
 def pengeluaran_instalasi():
-    return jsonify({'response': 'ini data pengeluaran instalasi'})
+    tgl_awal = request.args.get('tgl_awal')
+    tgl_akhir = request.args.get('tgl_akhir')
+    tgl_awal, tgl_akhir = get_default_date(tgl_awal, tgl_akhir)
+    result = engine.execute(
+        text(
+            f"""SELECT sbkk.TglBKK, i.NamaInstalasi, sbkk.JmlBayar
+           FROM rsudtasikmalaya.dbo.StrukBuktiKasKeluar sbkk
+           INNER JOIN rsudtasikmalaya.dbo.Ruangan r
+           ON sbkk.KdRuangan = r.KdRuangan
+           INNER JOIN rsudtasikmalaya.dbo.Instalasi i
+           ON r.KdInstalasi = i.KdInstalasi
+           WHERE sbkk.TglBKK >= '{tgl_awal}'
+           AND sbkk.TglBKK < '{tgl_akhir + timedelta(days=1)}'
+           ORDER BY sbkk.TglBKK ASC;"""))
+    data = []
+    for row in result:
+        data.append({
+            "tanggal": row['TglBKK'],
+            "instalasi": row['NamaInstalasi'],
+            "total": row['JmlBayar'],
+            "judul": 'Pengeluaran Instalasi',
+            "label": 'Pengeluaran'
+        })
+    cnt = Counter()
+    for i in range(len(data)):
+        cnt[data[i]['instalasi'].lower().replace(' ', '_')] += float(data[i]['total'])
+
+    result = {
+        "judul": 'Pengeluaran Instalasi',
+        "label": 'Pengeluaran',
+        "instalasi": cnt,
+        "tgl_filter": {"tgl_awal": tgl_awal, "tgl_akhir": tgl_akhir}
+    }
+    return jsonify(result)
 
 
 @pengeluaran_bp.route('/pengeluaran_rekanan')
 def pengeluaran_rekanan():
-    return jsonify({'response': 'ini data pengeluaran rekanan'})
+    tgl_awal = request.args.get('tgl_awal')
+    tahun = datetime.now().year if tgl_awal == None else int(tgl_awal[:4])
+    result = engine.execute(
+        text(
+            f"""SELECT spp.TglStruk, spp.IdPenjamin, p.NamaPenjamin, 
+            spp.TotalBiaya as Pengajuan, JmlHutangPenjamin as Klaim
+            FROM rsudtasikmalaya.dbo.StrukPelayananPasien spp
+            INNER JOIN rsudtasikmalaya.dbo.Penjamin p
+            ON spp.IdPenjamin = p.IdPenjamin
+            WHERE spp.IdPenjamin != 2222222222
+            AND datepart(year,[TglStruk]) = {tahun-1}
+            OR datepart(year,[TglStruk]) = {tahun}
+            ORDER BY spp.TglStruk ASC;"""))
+
+    tren = {}
+    for i in range(1, 13):
+        tren[month_id[i]] = {
+            "pengajuan": 0,
+            "klaim": 0,
+        }
+
+    for row in result:
+        curr_m = month_id[row['TglStruk'].month]
+        tren[curr_m]['pengajuan'] = round(tren[curr_m]['pengajuan'] + float(row['Pengajuan']), 2)
+        tren[curr_m]['klaim'] = round(tren[curr_m]['klaim'] + float(row['Klaim']), 2)
+        
+    data = {
+        "judul": "Pengeluaran Rekanan",
+        "label": 'Pengeluaran',
+        "tahun": tahun,
+        "asuransi": tren
+    }
+    return jsonify(data)
 
 
 @pengeluaran_bp.route('/pengeluaran_produk')
 def pengeluaran_produk():
-    return jsonify({'response': 'ini date pengeluaran produk'})
+    return jsonify({'response': 'ini data pengeluaran produk'})
 
 
 @pengeluaran_bp.route('/pengeluaran_cara_bayar')
@@ -122,7 +186,8 @@ def pengeluaran_cara_bayar():
         })
     cnt = Counter()
     for i in range(len(data)):
-        cnt[data[i]['cara_bayar'].lower().replace(' ', '_')] += float(data[i]['total'])
+        cnt[data[i]['cara_bayar'].lower().replace(
+            ' ', '_')] += float(data[i]['total'])
 
     result = {
         "judul": 'Pengeluaran Cara Bayar',
