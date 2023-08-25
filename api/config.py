c3f38c87c0113a30ce706e545b1da66e5126879b
gunjou <gjoefandi@gmail.com>
2023-08-25 11:01:39 +0700

    last update

--- api/config.py
+++ api/config.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 MONTH_ID = ["", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
 
 def get_connection():
-    server = '192.168.0.250\ss2008r2'
-    database = 'rsudtasikmalaya'
+    server = '192.168.0.250\ss2022'
+    database = 'rsud_tasikmalaya'
     username = 'sa'
     password = 'j4s4medik4'
 
